1c8fbf39ccac0d830a0661e8af45851cd29b4d9a
Thirupathi309 <thirupathikathi99@gmail.com>
2025-10-18 20:15:46 +0530

    adding Java.java file

---
+++ Java.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Java {
+	public static void main(String[] args) {
+		
+		List<Integer>numbers=Arrays.asList(33,22,55,44,77,88,50);
+		numbers.stream().filter(i->i%2!=0).forEach(n->System.out.println(n));
+	}
+}
+
+
